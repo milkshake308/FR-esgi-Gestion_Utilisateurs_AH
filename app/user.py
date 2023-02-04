64251d6c0cc59e6e4728f2a3d0381489735a13a7
Aaron <rnsaadoun@hotmail.com>
2023-02-04 09:24:43 +0100

    compactage des modules utilisateurs en un fichier

--- app/user.py
+++ app/user.py
@@ -1,12 +1,30 @@
-import login
-import passwd
+import random, hashlib
+
+#  variable qui traque l'utilisateur actuellement connecté
+
+current_user = None
+def sha256_generator(str):
+    m = hashlib.sha256()
+    m.update(str.encode())
+
+    return m.hexdigest()
+
+def generate_login(p_nom, p_prenom):
+    p_nom = p_nom.replace(" ", "")
+    p_prenom = p_prenom.strip()
+    return p_prenom[0]+p_nom
+
+def generate_passwd():
+    element = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+-*/~$%&.:?!@"
+    passwd = ""
+
+    for i in range(9): passwd = passwd + element[random.randint(0, len(element) - 1)]
+    passwd_hash = sha256_generator(passwd)
+    return passwd
+
 def user_add():
     nom = input("Entrez votre nom svp : ")
     prenom = input("Entrez votre prenom svp : ")
-    passwds = passwd.generate_passwd()
-    print("Votre login est : " + login.generate_login(nom, prenom))
-    print("Votre Mot de passe est : " + passwds[0])
-    print("Le hash du mot de passe est : " + passwds[1])
-
-
-user_add()
+    passwd = generate_passwd()
+    login = generate_login(nom, prenom)
+    return nom, prenom, passwd, login
